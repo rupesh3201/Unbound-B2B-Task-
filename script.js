4d806b3b41c8089b543a4812ce3288cae2b44dc6
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-18 05:55:54 +0530

    on submit  event  from read here

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('myForm');
 
-    
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        
+        // Get form field values
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const companyName = document.getElementById('companyName').value;
+        const jobTitle = document.getElementById('firstName').value; // 
+        const companySize = document.getElementById('companySize').value;
+        const country = document.getElementById('country').value;
+        const industry = document.getElementById('industry').value;
+        const newsletter = document.getElementById('newsletter').checked;
+        const terms = document.getElementById('terms').checked;
+
+   
+    });
 });
